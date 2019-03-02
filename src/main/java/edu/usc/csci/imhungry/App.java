a2a130d03e997b61b3566abc7e54c71b8788f2b3
ivanightingale <guancheq@usc.edu>
2019-03-02 11:12:50 -0800

    initial commit

---
+++ src/main/java/edu/usc/csci/imhungry/App.java
@@ -0,0 +1,13 @@
+package edu.usc.csci.imhungry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
