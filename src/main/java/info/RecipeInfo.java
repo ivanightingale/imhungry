0cccb1ff482bd40ecb109db24db829809ca7bf58
Tong Wu <twu781@usc.edu>
2019-03-05 22:01:10 -0800

    update doGet in searchServlet

--- src/main/java/info/RecipeInfo.java
+++ src/main/java/info/RecipeInfo.java
@@ -3,62 +3,29 @@ package info;
 import java.util.ArrayList;
 
 public class RecipeInfo extends Info implements Comparable<RecipeInfo> {
-<<<<<<< HEAD
-<<<<<<< HEAD
-	private String name;
-	private String prepTime;
-	private String cookTime;
-	private String ingredients;
-	private String instructions;
-	
-	public RecipeInfo(String name, String prepTime, String cookTime, String ingredients,
-=======
+
+
 	//RecipeInfo objects each store information of a recipe.
 	public int prepTime;
 	public int cookTime;
 	public ArrayList<String> ingredients;
 	public String instructions;  //FIXME
-	
+
 	public RecipeInfo(String name, double rating, int prepTime, int cookTime, ArrayList<String> ingredients,
->>>>>>> 3668ec296f94964d5697cc559239c44490768a77
-=======
-	//RecipeInfo objects each store information of a recipe.
-	public int prepTime;
-	public int cookTime;
-	public ArrayList<String> ingredients;
-	public String instructions;  //FIXME
-	
-	public RecipeInfo(String name, double rating, int prepTime, int cookTime, ArrayList<String> ingredients,
->>>>>>> e1477d2cc702d72b39874e28c2f215818421992e
-			String instructions) {
+
+
+	public RecipeInfo(String name, double rating, int prepTime, int cookTime, ArrayList<String> ingredients,String instructions) {
 		this.name = name;
 		this.prepTime = prepTime;
 		this.cookTime = cookTime;
 		this.ingredients = ingredients;
 		this.instructions = instructions;
 	}
-<<<<<<< HEAD
-	
-<<<<<<< HEAD
-	public String getName() {return name;}
-	
-	public String getPrepTime() { return prepTime; }
-	
-	public String getCookTime() { return cookTime; }
-	
-	public String getIngredients() { return ingredients; }
-	
-	public String getInstructions() { return instructions; }
-	
-	public boolean equals(RecipeInfo other) {
-		return this.name == other.name && this.prepTime == other.prepTime && this.cookTime == other.cookTime;
-=======
 
 	public int compareTo(RecipeInfo other) {
 		return this.prepTime - other.prepTime;
->>>>>>> e1477d2cc702d72b39874e28c2f215818421992e
 	}
-	
+
 	@Override
 	public boolean equals(Object other) {
 		if(other == this) return true;
@@ -68,18 +35,3 @@ public class RecipeInfo extends Info implements Comparable<RecipeInfo> {
 				this.cookTime == otherRecipeInfo.cookTime;
 	}
 }
-=======
-	public int compareTo(RecipeInfo other) {
-		return this.prepTime - other.prepTime;
-	}
-	
-	@Override
-	public boolean equals(Object other) {
-		if(other == this) return true;
-		if(!(other instanceof RecipeInfo)) return false;
-		RecipeInfo otherRecipeInfo = (RecipeInfo) other;
-		return this.name.equals(otherRecipeInfo.name) && this.prepTime == otherRecipeInfo.prepTime &&
-				this.cookTime == otherRecipeInfo.cookTime;
-	}
-}
->>>>>>> 3668ec296f94964d5697cc559239c44490768a77
--- src/main/java/info/RestaurantInfo.java
+++ src/main/java/info/RestaurantInfo.java
@@ -1,22 +1,5 @@
 package info;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-public class RestaurantInfo extends Info {
-	private String name;
-	private String address;
-	private String price;
-	private int driveTime;
-	private int rating;
-	private String phone;
-	private String url;
-	
-	
-	public RestaurantInfo(String name, int rating, String address, String price, int driveTime, String phone,
-			String url) {
-=======
-=======
->>>>>>> e1477d2cc702d72b39874e28c2f215818421992e
 public class RestaurantInfo extends Info implements Comparable<RestaurantInfo> {
 	//RestaurantInfo objects each store information of a restaurant.
 	public String placeID;
@@ -26,14 +9,11 @@ public class RestaurantInfo extends Info implements Comparable<RestaurantInfo> {
 	public int driveTimeValue;
 	public String phone;
 	public String url;
-	
-	
+
+
 	public RestaurantInfo(String name, double rating, String placeID, String address, int price, String driveTimeText,
 			int driveTimeValue, String phone, String url) {
-<<<<<<< HEAD
->>>>>>> 3668ec296f94964d5697cc559239c44490768a77
-=======
->>>>>>> e1477d2cc702d72b39874e28c2f215818421992e
+
 		this.name = name;
 		this.rating = rating;
 		this.placeID = placeID;
@@ -44,43 +24,17 @@ public class RestaurantInfo extends Info implements Comparable<RestaurantInfo> {
 		this.phone = phone;
 		this.url = url;
 	}
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public String getAddress() { return address; }
-	
-	public String getPrice() { return price; }
-	
-	public int getDriveTime() { return driveTime; }
-	
-	public String getPhone() { return phone; }
-	
-	public String getURL() {return url; }
-	
-	public int getRating() {return rating;}
-	
-	public boolean equals(RestaurantInfo other) {
-		return this.name == other.name && this.address == other.address;
-=======
+
 	public int compareTo(RestaurantInfo other) {
 		return this.driveTimeValue - other.driveTimeValue;
 	}
-	
-=======
-	public int compareTo(RestaurantInfo other) {
-		return this.driveTimeValue - other.driveTimeValue;
-	}
-	
->>>>>>> e1477d2cc702d72b39874e28c2f215818421992e
+
+
 	@Override
 	public boolean equals(Object other) {
 		if(other == this) return true;
 		if(!(other instanceof RestaurantInfo)) return false;
 		RestaurantInfo otherRestaurantInfo = (RestaurantInfo) other;
 		return this.name.equals(otherRestaurantInfo.name) && this.placeID.equals(otherRestaurantInfo.placeID);
-<<<<<<< HEAD
->>>>>>> 3668ec296f94964d5697cc559239c44490768a77
-=======
->>>>>>> e1477d2cc702d72b39874e28c2f215818421992e
 	}
 }
