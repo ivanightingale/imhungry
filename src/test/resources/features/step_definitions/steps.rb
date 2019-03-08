d08822733f21fb973a94f50c13eebadcc25e9673
ivanightingale <guancheq@usc.edu>
2019-03-08 06:32:56 -0800

    Update steps.rb

--- src/test/resources/features/step_definitions/steps.rb
+++ src/test/resources/features/step_definitions/steps.rb
@@ -34,7 +34,7 @@ end
 
 When(/^select the list "([^"]*)"$/) do |listName|
     find('.select-selected').click
-    first(:text, listName).click
+    first('div', :text => listName).click
 end
 
 Then(/^I should see an info item$/) do
