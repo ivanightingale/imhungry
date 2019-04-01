91e231f84cc4d2d8ae2d28e42d057752d4b7b78c
Luke St. Regis <stregis@usc.edu>
2019-04-01 02:41:46 -0700

    Password hashing implementation

--- src/main/java/security/PasswordHashing.java
+++ src/main/java/security/PasswordHashing.java
@@ -10,30 +10,40 @@ import java.util.Base64;
 
 public class PasswordHashing {
     private static final SecureRandom RAND = new SecureRandom();
-    private static String salt = "[B@8e24743";
     private static final int ITERATIONS = 65536;
     private static final int KEY_LENGTH = 512;
     private static final String ALGORITHM = "PBKDF2WithHmacSHA512";
 
-    public String hashPassword(String password) {
+    public static String hashPassword(String password, String salt) {
         char[] chars = password.toCharArray();
         byte[] bytes = salt.getBytes();
 
-        PBEKeySpec spec = new PBEKeySpec(chars, bytes, ITERATIONS, KEY_LENGTH);
-
-        Arrays.fill(chars, Character.MIN_VALUE);
-
         try {
+            PBEKeySpec spec = new PBEKeySpec(chars, bytes, ITERATIONS, KEY_LENGTH);
+
+            Arrays.fill(chars, Character.MIN_VALUE);
+
             SecretKeyFactory fac = SecretKeyFactory.getInstance(ALGORITHM);
             byte[] securePassword = fac.generateSecret(spec).getEncoded();
+
+            spec.clearPassword();
+
             return Base64.getEncoder().encodeToString(securePassword);
 
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+        } catch (IllegalArgumentException | NoSuchAlgorithmException | InvalidKeySpecException ex) {
             System.err.println("Exception encountered in hashPassword()");
             return "";
 
-        } finally {
-            spec.clearPassword();
         }
+    }
+
+    public static String getRandomSalt() {
+        String saltchars = "ABCDEFGHIJKLMNOPQRSTUVabcdefghijklmnopqrstuvwxyz1234567890+/";
+        StringBuilder salt = new StringBuilder();
+        while (salt.length() < 86) {
+            int index = (int) (RAND.nextDouble() * saltchars.length());
+            salt.append(saltchars.charAt(index));
+        }
+        return salt.toString();
     }
 }
