5092aff7455470a902a786a8d7f84b5a4e455d70
David <99davidpaul@gmail.com>
2019-04-07 16:57:25 -0700

    Implemented testing for SortList.java class

--- src/main/java/info/SortLists.java
+++ src/main/java/info/SortLists.java
@@ -1,4 +1,17 @@
 package info;
 
+import java.util.ArrayList;
+
 public class SortLists {
+    public static ArrayList<Info> sortAlphabetically(ArrayList<Info> list) {
+        return null;
+    }
+
+    public static ArrayList<Info> sortByDriveTime(ArrayList<Info> list){
+        return null;
+    }
+
+    public static ArrayList<Info> sortByPrice(ArrayList<Info> list) {
+        return null;
+    }
 }
--- src/test/java/info/SortListsTest.java
+++ src/test/java/info/SortListsTest.java
@@ -1,4 +1,105 @@
 package info;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class SortListsTest {
+    ArrayList<Info> actual;
+    @Before
+    public void initialize() {
+        actual = new ArrayList<Info>();
+    }
+
+    // tests sorting alphabetically
+    @Test
+    public void sortAlphabeticallyTest() {
+        // tests if out of order
+        actual.add(new RestaurantInfo("McDonald's", 1, "1", "1", 1, "1", 1, "1", "1"));
+        actual.add(new RestaurantInfo("Wendy's", 1, "1", "1", 1, "1", 1, "1", "1"));
+        actual.add(new RestaurantInfo("Burger King", 1, "1", "1", 1, "1", 1, "1", "1"));
+
+        actual = SortLists.sortAlphabetically(actual);
+        assertEquals("Burger King", actual.get(0).name);
+        assertEquals("McDonald's", actual.get(1).name);
+        assertEquals("Wendy's", actual.get(2).name);
+
+        // tests if in order
+        actual = new ArrayList<Info>();
+        actual.add(new RestaurantInfo("Burger King", 1, "1", "1", 1, "1", 1, "1", "1"));
+        actual.add(new RestaurantInfo("McDonald's", 1, "1", "1", 1, "1", 1, "1", "1"));
+        actual.add(new RestaurantInfo("Wendy's", 1, "1", "1", 1, "1", 1, "1", "1"));
+
+        actual = SortLists.sortAlphabetically(actual);
+        assertEquals("Burger King", actual.get(0).name);
+        assertEquals("McDonald's", actual.get(1).name);
+        assertEquals("Wendy's", actual.get(2).name);
+    }
+/*
+    // tests sorting by distance
+    @Test
+    public void sortByDriveTimeTest() {
+        // tests if out of order
+        actual.add(new RestaurantInfo("McDonald's", 1, "1", "1", 1, "10", 10, "1", "1"));
+        actual.add(new RestaurantInfo("Wendy's", 1, "1", "1", 1, "15", 15, "1", "1"));
+        actual.add(new RestaurantInfo("Burger King", 1, "1", "1", 1, "5", 5, "1", "1"));
+
+        actual = SortLists.sortByDriveTime(actual);
+        RestaurantInfo ri;
+        ri = (RestaurantInfo) actual.get(0);
+        assertEquals(5, ri.driveTimeValue);
+        ri = (RestaurantInfo) actual.get(1);
+        assertEquals(10, ri.driveTimeValue);
+        ri = (RestaurantInfo) actual.get(2);
+        assertEquals(15, ri.driveTimeValue);
+
+        // tests if in order
+        actual = new ArrayList<Info>();
+        actual.add(new RestaurantInfo("McDonald's", 1, "1", "1", 1, "10", 5, "1", "1"));
+        actual.add(new RestaurantInfo("Wendy's", 1, "1", "1", 1, "15", 10, "1", "1"));
+        actual.add(new RestaurantInfo("Burger King", 1, "1", "1", 1, "5", 15, "1", "1"));
+
+        actual = SortLists.sortByDriveTime(actual);
+        ri = (RestaurantInfo) actual.get(0);
+        assertEquals(5, ri.driveTimeValue);
+        ri = (RestaurantInfo) actual.get(1);
+        assertEquals(10, ri.driveTimeValue);
+        ri = (RestaurantInfo) actual.get(2);
+        assertEquals(15, ri.driveTimeValue);
+    }
+
+    // tests sorting by price
+    @Test
+    public void sortByPriceTest() {
+        // tests if out of order
+        actual.add(new RestaurantInfo("McDonald's", 1, "1", "1", 2, "1", 1, "1", "1"));
+        actual.add(new RestaurantInfo("Wendy's", 1, "1", "1", 3, "1", 1, "1", "1"));
+        actual.add(new RestaurantInfo("Burger King", 1, "1", "1", 1, "1", 1, "1", "1"));
+
+        actual = SortLists.sortByDriveTime(actual);
+        RestaurantInfo ri;
+        ri = (RestaurantInfo) actual.get(0);
+        assertEquals(1, ri.priceLevel);
+        ri = (RestaurantInfo) actual.get(1);
+        assertEquals(2, ri.priceLevel);
+        ri = (RestaurantInfo) actual.get(2);
+        assertEquals(3, ri.priceLevel);
+
+        // tests if in order
+        actual = new ArrayList<Info>();
+        actual.add(new RestaurantInfo("McDonald's", 1, "1", "1", 1, "10", 5, "1", "1"));
+        actual.add(new RestaurantInfo("Wendy's", 1, "1", "1", 2, "15", 10, "1", "1"));
+        actual.add(new RestaurantInfo("Burger King", 1, "1", "1", 3, "5", 15, "1", "1"));
+
+        actual = SortLists.sortByPrice(actual);
+        ri = (RestaurantInfo) actual.get(0);
+        assertEquals(1, ri.driveTimeValue);
+        ri = (RestaurantInfo) actual.get(1);
+        assertEquals(2, ri.driveTimeValue);
+        ri = (RestaurantInfo) actual.get(2);
+        assertEquals(3, ri.driveTimeValue);
+    }*/
 }
